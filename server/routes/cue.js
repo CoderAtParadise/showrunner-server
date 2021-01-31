22c172d0e1490c9a76fd688d4cead045faf5a31b
CoderAtParadise <dawn0966@gmail.com>
2021-01-31 12:22:36 +1000

    Prettifier fix

--- server/routes/cue.js
+++ server/routes/cue.js
@@ -4,21 +4,36 @@ const eventhandler = require("../components/event");
 
 router.get("/cues", async (req, res) => {
   let targets = [];
+  if (req.query.target) targets = req.query.target.split(",");
+  //prettier-ignore
   res.set({
     "Cache-Control": "no-cache",
     "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
+    "Connection": "keep-alive",
   });
   res.flushHeaders();
-  res.write("retry: 10000\n\n");
   eventhandler.on("cue", (target, message) => {
     if (targets.length === 0 || targets.has(target))
       res.write(
-        `event: cue:\n target: ${target}\ndata:${JSON.stringify(message)}\n\n`
+        `event: cue\ntarget: ${target}\ndata: ${JSON.stringify(message)}\n\n`
       );
   });
 });
 
+//cue/trigger?item=0,cue=1
 router.get("/cue/trigger", (req, res, next) => {});
+
+router.get("/prompt", (req, res) => {
+  //prettier-ignore
+  res.set({
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream",
+    "Connection": "keep-alive",
+  });
+  res.flushHeaders();
+  eventhandler.on("itemswitch", () => {
+    //res.write(`event: item\ndata:${0}\n\n`);
+  });
+});
 
 module.exports = router;
--- server/routes/timer.js
+++ server/routes/timer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { getTimer,getTimers,Timepoint } = require("../components/timer");
-const eventhandler = require('../components/event');
+const { getTimer, getTimers, Timepoint } = require("../components/timer");
+const eventhandler = require("../components/event");
 
 const sendTimers = (map) => {
   let obj = { clock: Timepoint.stringify(Timepoint.now()), timers: [] };
@@ -10,21 +10,19 @@ const sendTimers = (map) => {
 };
 
 router.get("/timers", async (req, res) => {
+  //prettier-ignore
   res.set({
     "Cache-Control": "no-cache",
     "Content-Type": "text/event-stream",
-    'Connection': "keep-alive",
+    "Connection": "keep-alive",
   });
   res.flushHeaders();
-  res.write("retry: 10000\n\n");
   eventhandler.on("timer", () => {
-      res.write(`event: timer\ndata: ${sendTimers(getTimers)}\n\n`);
+    res.write(`event: timer\ndata: ${sendTimers(getTimers)}\n\n`);
   });
 });
 
-router.get("/timer/create",(req,res,next) => {
-
-});
+router.post("/timer/create", (req, res, next) => {});
 
 router.get("/timer/:id", (req, res, next) => {
   let command = req.query.command;
@@ -38,30 +36,40 @@ router.get("/timer/:id", (req, res, next) => {
   if (!timer) {
     res
       .status(404)
-      .json({ error: true, message: `No timer exists with id: ${req.params.id}` });
+      .json({
+        error: true,
+        message: `No timer exists with id: ${req.params.id}`,
+      });
     return;
   }
   switch (command) {
     case "start":
       timer.start();
-      res.status(200).json({ error: false, message: `Started timer: ${req.params.id}}` });
+      res
+        .status(200)
+        .json({ error: false, message: `Started timer: ${req.params.id}}` });
       break;
     case "stop":
       timer.stop();
       res.status(200).json({ message: `Stopped timer: ${req.params.id}` });
       break;
     case "reset":
-        timer.reset();
-        res.status(200).json({message: `Reset timer: ${req.params.id}` });
-        break;
+      timer.reset();
+      res.status(200).json({ message: `Reset timer: ${req.params.id}` });
+      break;
     case "restart":
-        timer.restart();
-        res.status(200).json({ message: `Restarted timer: ${req.params.id}` });
-        break;
+      timer.restart();
+      res.status(200).json({ message: `Restarted timer: ${req.params.id}` });
+      break;
     case "delete":
-        break;
+      break;
     default:
-        res.status(400).json({error:true,message: `Unknown Timer Command: ${req.params.id}`});
+      res
+        .status(400)
+        .json({
+          error: true,
+          message: `Unknown Timer Command: ${req.params.id}`,
+        });
   }
 });
 
