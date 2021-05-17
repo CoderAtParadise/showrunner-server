a5e9feccecdcdd27a3d1cdb7ec12b5c7579deb98
CoderAtParadise <dawn0966@gmail.com>
2021-05-18 00:21:56 +1000

    delete works

--- app.ts
+++ app.ts
@@ -30,26 +30,3 @@ app.listen(port, () => {
   debug(`Running at http://localhost:${port}`);
 });
 init(eventhandler); //Pass into ControlHandler becuase for some reason eventhandler is losing data
-
-schedule(() => {
-  LoadRunsheet({
-    command: "loadRunsheet",
-    session: "",
-    tracking_id: "",
-    data: "temp",
-  });
-  schedule(() => {
-    Goto({
-      command: "goto",
-      session: "3c8735e6-b536-419c-8950-9284116e50a2",
-      tracking_id: "e213a399-a633-47cd-bcbf-6f39bc1d5014",
-    });
-    schedule(() => {
-      Goto({
-        command: "goto",
-        session: "3c8735e6-b536-419c-8950-9284116e50a2",
-        tracking_id: "1d7db0a7-7787-444a-bbd7-6efbeb7041cf",
-      });
-    });
-  });
-});
--- components/server/Control.ts
+++ components/server/Control.ts
@@ -1,5 +1,9 @@
-import { RunsheetStorage, JSON as RJSON } from "../common/Runsheet";
-import { Storage, Nested, get, Type } from "../common/Storage";
+import {
+  RunsheetStorage,
+  JSON as RJSON,
+  INVALID as INVALID_RUNSHEET,
+} from "../common/Runsheet";
+import { Storage, Nested, get, Type, remove } from "../common/Storage";
 import "./Messages";
 import "./Triggers";
 import fs from "fs";
@@ -16,7 +20,7 @@ import Debug from "debug";
 import EventEmitter from "events";
 import { addThisTickHandler } from "./Eventhandler";
 import { SessionStorage } from "../common/Session";
-import { now, add, Point, subtract, greaterThan, equals } from "../common/Time";
+import { now, add, Point, subtract, equals } from "../common/Time";
 import { Behaviour, TimerState } from "../common/Timer";
 
 export const ControlHandler: {
@@ -51,8 +55,11 @@ export function init(eventhandler: EventEmitter) {
       if (session) {
         const tracker = session.trackers.get(ControlHandler.current.active);
         if (tracker && tracker.index !== -1) {
-          const time = subtract(now(),tracker.timers[tracker.index].start);
-          if (equals(time, tracker.settings.duration) && tracker.timers[tracker.index].state === TimerState.RUNNING) {
+          const time = subtract(now(), tracker.timers[tracker.index].start);
+          if (
+            equals(time, tracker.settings.duration) &&
+            tracker.timers[tracker.index].state === TimerState.RUNNING
+          ) {
             switch (tracker.settings.behaviour) {
               case Behaviour.OVERRUN:
                 tracker.timers[tracker.index].state = TimerState.OVERRUN;
@@ -96,7 +103,11 @@ export function StartSession(session: string): void {
       if (tracker) {
         tracker.timer.start = now();
         tracker.timer.end = add(now(), tracker.settings.duration);
-        ControlHandler.eventhandler?.emit("sync", "tracking_session", TSJSON.serialize(tracker));
+        ControlHandler.eventhandler?.emit(
+          "sync",
+          "tracking_session",
+          TSJSON.serialize(tracker)
+        );
       }
     } else ControlHandler.current.session = "";
   }
@@ -107,7 +118,11 @@ export function EndSession(session: string): void {
     const tracker = ControlHandler.tracking.get(session);
     if (tracker) {
       tracker.timer.end = now();
-      ControlHandler.eventhandler?.emit("sync", "tracking_session", TSJSON.serialize(tracker));
+      ControlHandler.eventhandler?.emit(
+        "sync",
+        "tracking_session",
+        TSJSON.serialize(tracker)
+      );
     }
     ControlHandler.current.session = "";
     ControlHandler.current.active = "";
@@ -115,29 +130,118 @@ export function EndSession(session: string): void {
   }
 }
 
-export function Disable(command:Command) :void {
-  if (ControlHandler.loaded)
-  {
-    
+export function Disable(command: Command): void {
+  if (ControlHandler.loaded) {
+    console.log(command.session);
+    const tsession = ControlHandler.tracking.get(command.session);
+    if (tsession) {
+      if (command.tracking_id === "") {
+        console.log("Hello");
+        tsession.disabled = !tsession.disabled;
+        ControlHandler.eventhandler?.emit(
+          "sync",
+          "tracking_session",
+          TSJSON.serialize(tsession)
+        );
+      }
+    } else {
+    }
+  }
+}
+
+export function Delete(command: Command): void {
+  if (ControlHandler.loaded) {
+    ControlHandler.eventhandler?.emit(
+      "sync",
+      "runsheet",
+      RJSON.serialize(INVALID_RUNSHEET)
+    );
+    const tsession = ControlHandler.tracking.get(command.session);
+    if (tsession) {
+      if (tsession.tracking_id === command.tracking_id) {
+        const session = get(
+          ControlHandler.loaded,
+          command.tracking_id
+        ) as SessionStorage;
+        ControlHandler.tracking.delete(command.session);
+        let di: number = -1;
+        session.start.forEach((v: { session_id: string }, i: number) => {
+          if (v.session_id === command.session) {
+            di = i;
+            return;
+          }
+        });
+        if (session.start.length === 1)
+          remove(ControlHandler.loaded, command.tracking_id);
+        session.start.splice(di, 1);
+      } else {
+        const session = get(
+          ControlHandler.loaded,
+          tsession.tracking_id
+        ) as SessionStorage;
+
+        const t = tsession.trackers.get(command.tracking_id);
+        console.log(t?.parent);
+        if (t && t.parent === session.tracking) {
+          const bracket = get(session, command.tracking_id) as Storage & Nested;
+          session.start.forEach((v: { session_id: string }) => {
+            const ts = ControlHandler.tracking.get(v.session_id);
+            bracket.index.forEach((v: string) => {
+              ts?.trackers.delete(v);
+            });
+            ts?.trackers.delete(command.tracking_id);
+          });
+          remove(session, command.tracking_id);
+        } else if (t && t.parent !== session.tracking) {
+          const bracket = get(session, t.parent) as Storage & Nested;
+          session.start.forEach((v: { session_id: string }) => {
+            const ts = ControlHandler.tracking.get(v.session_id);
+            ts?.trackers.delete(command.tracking_id);
+          });
+          remove(bracket, command.tracking_id);
+        }
+      }
+    }
+    ControlHandler.eventhandler?.emit(
+      "sync",
+      "runsheet",
+      RJSON.serialize(ControlHandler.loaded)
+    );
+    const tracking_list: object[] = [];
+    ControlHandler.tracking.forEach((value: TrackingSession) =>
+      tracking_list.push(TSJSON.serialize(value))
+    );
+    ControlHandler.eventhandler?.emit("sync", "tracking_list", tracking_list);
   }
 }
 
 export function Goto(command: Command): void {
   if (ControlHandler.loaded) {
-    if (ControlHandler.current.session !== command.session || command.tracking_id === "start") {
+    if (
+      ControlHandler.current.session !== command.session ||
+      command.tracking_id === "start"
+    ) {
       const session = ControlHandler.tracking.get(
         ControlHandler.current.session
       );
       if (session) {
         const item = session.trackers.get(ControlHandler.current.active);
-        if (item && (item.timers[item.index].state === TimerState.RUNNING || item.timers[item.index].state == TimerState.OVERRUN)) {
+        if (
+          item &&
+          (item.timers[item.index].state === TimerState.RUNNING ||
+            item.timers[item.index].state == TimerState.OVERRUN)
+        ) {
           end(item);
           ControlHandler.eventhandler?.emit("sync", "tracking", {
             session: ControlHandler.current.session,
             tracker: TJSON.serialize(item),
           });
           const bracket = session.trackers.get(item.parent);
-          if (bracket && (bracket.timers[bracket.index].state === TimerState.RUNNING || bracket.timers[bracket.index].state == TimerState.OVERRUN)) {
+          if (
+            bracket &&
+            (bracket.timers[bracket.index].state === TimerState.RUNNING ||
+              bracket.timers[bracket.index].state == TimerState.OVERRUN)
+          ) {
             end(bracket);
             ControlHandler.eventhandler?.emit("sync", "tracking", {
               session: ControlHandler.current.session,
@@ -154,28 +258,37 @@ export function Goto(command: Command): void {
       let goto = session.trackers.get(command.tracking_id);
       if (goto) {
         const active = session.trackers.get(ControlHandler.current.active);
-        if (active && (active.timers[active.index].state === TimerState.RUNNING || active.timers[active.index].state == TimerState.OVERRUN)) {
-          end(active);
-          ControlHandler.eventhandler?.emit("sync", "tracking", {
-            session: command.session,
-            tracker: TJSON.serialize(active),
-          });
-          if (goto.parent !== active.parent) {
-            const parent = session.trackers.get(active.parent);
-            if (parent && (parent.timers[parent.index].state === TimerState.RUNNING || parent.timers[parent.index].state == TimerState.OVERRUN)) {
-              end(parent);
-              ControlHandler.eventhandler?.emit("sync", "tracking", {
-                session: command.session,
-                tracker: TJSON.serialize(parent),
-              });
+        if (active) {
+          if (
+            active.timers[active.index].state === TimerState.RUNNING ||
+            active.timers[active.index].state == TimerState.OVERRUN
+          ) {
+            end(active);
+            ControlHandler.eventhandler?.emit("sync", "tracking", {
+              session: command.session,
+              tracker: TJSON.serialize(active),
+            });
+            if (goto.parent !== active.parent) {
+              const parent = session.trackers.get(active.parent);
+              if (
+                parent &&
+                (parent.timers[parent.index].state === TimerState.RUNNING ||
+                  parent.timers[parent.index].state == TimerState.OVERRUN)
+              ) {
+                end(parent);
+                ControlHandler.eventhandler?.emit("sync", "tracking", {
+                  session: command.session,
+                  tracker: TJSON.serialize(parent),
+                });
+              }
             }
           }
         }
         if (command.tracking_id !== "") {
           if (goto.parent === session.tracking_id) {
             const ss = get(ControlHandler.loaded, session.tracking_id);
-            const bs = get((ss as unknown) as Nested, goto.tracking_id);
-            const next = getNextEnabled((bs as unknown) as Nested, -1);
+            const bs = get(ss as unknown as Nested, goto.tracking_id);
+            const next = getNextEnabled(bs as unknown as Nested, -1);
             if (next) {
               const tracker = session.trackers.get(next.tracking);
               if (tracker) {
@@ -197,8 +310,8 @@ export function Goto(command: Command): void {
             }
           } else {
             const ss = get(ControlHandler.loaded, session.tracking_id);
-            const bs = get((ss as unknown) as Nested, goto.parent);
-            const next = get((bs as unknown) as Nested, goto.tracking_id);
+            const bs = get(ss as unknown as Nested, goto.parent);
+            const next = get(bs as unknown as Nested, goto.tracking_id);
             if (next) {
               const tracker = session.trackers.get(next.tracking);
               if (tracker) {
@@ -222,8 +335,7 @@ export function Goto(command: Command): void {
             }
           }
         }
-      }
-      else {
+      } else {
         ControlHandler.current.active = "";
       }
       ControlHandler.current.next = getNext();
@@ -256,14 +368,14 @@ function getNext(): string {
     const tracking: string = trackingSession?.tracking_id || "";
     const storage: Storage = get(ControlHandler.loaded, tracking);
     if (storage.type !== Type.INVALID) {
-      const session = (storage as unknown) as Nested;
+      const session = storage as unknown as Nested;
       if (ControlHandler.current.active === "") {
         let index: number = -1;
         while (index < session.index.length) {
-          let bracket: Nested = (getNextEnabled(
+          let bracket: Nested = getNextEnabled(
             session,
             index
-          ) as unknown) as Nested;
+          ) as unknown as Nested;
           if (bracket) {
             let bindex: number = -1;
             let item: Storage | null = getNextEnabled(bracket, bindex);
@@ -275,16 +387,16 @@ function getNext(): string {
           ControlHandler.current.active
         );
         if (active) {
-          let bracket: Nested = (session.nested.get(
+          let bracket: Nested = session.nested.get(
             active.parent
-          ) as unknown) as Nested;
+          ) as unknown as Nested;
           let index: number = bracket.index.indexOf(active.tracking_id);
           let item: Storage | null = getNextEnabled(bracket, index);
           if (item) return item.tracking;
           else {
             let bindex: number = session.index.indexOf(active.parent);
             while (bindex < session.index.length) {
-              bracket = (getNextEnabled(session, bindex) as unknown) as Nested;
+              bracket = getNextEnabled(session, bindex) as unknown as Nested;
               if (bracket) {
                 index = -1;
                 item = getNextEnabled(bracket, index);
@@ -302,6 +414,7 @@ function getNext(): string {
 
 export function LoadRunsheet(command: Command): void {
   const file = command.data as string;
+  Goto({ command: "goto", session: "", tracking_id: "" });
   loadRunsheet(file, (runsheet: any) => {
     if (runsheet.error) Debug("showrunner:io")(runsheet.message);
     else {
@@ -311,7 +424,7 @@ export function LoadRunsheet(command: Command): void {
         ControlHandler.loaded.nested.forEach((value: Storage) => {
           const session = value as SessionStorage;
           session.start.forEach(
-            (value: { session_id: string; time: Point }) => {
+            (value: { session_id: string; time: Point; disabled: boolean }) => {
               ControlHandler.tracking.set(
                 value.session_id,
                 buildTrackingSession(value, session)
@@ -348,16 +461,16 @@ const templateDir = "storage/templates";
 const knownRunsheets: Map<string, string> = new Map<string, string>();
 const knownTemplates: Map<string, string> = new Map<string, string>();
 
-export function RunsheetList() : string[] {
+export function RunsheetList(): string[] {
   return Array.from(knownRunsheets.keys());
 }
 
-export function TemplateList() : string[] {
+export function TemplateList(): string[] {
   return Array.from(knownTemplates.keys());
 }
 
 function Discover(dir: string, storage: Map<string, string>): void {
-  fs.mkdir(dir, () => { });
+  fs.mkdir(dir, () => {});
   const LoadDir = (
     dirPath: string,
     extension: string,
@@ -372,12 +485,14 @@ function Discover(dir: string, storage: Map<string, string>): void {
     });
   };
 
-  const filter = (dirPath: string, output: Map<string, string>): void =>
+  const filter = (dirPath: string, output: Map<string, string>): void => {
+    output.clear();
     LoadDir(dirPath, ".json", (err: Error | null, files: string[]) =>
       files.forEach((file: string) =>
         output.set(path.basename(file, ".json"), file)
       )
     );
+  };
   filter(dir, storage);
 }
 
